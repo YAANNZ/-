461cb5418830ab77044d920929c59359a9c623f0
YAANNZ <zhuynchn@163.com>
2018-05-28 23:37:20 +0800

    Home AFN

--- PositivePersons/Project/Home/ViewModel/PPSHomeTableViewModel.m
+++ PositivePersons/Project/Home/ViewModel/PPSHomeTableViewModel.m
@@ -10,4 +10,24 @@
 
 @implementation PPSHomeTableViewModel
 
+- (void)getAllOptionsData
+{
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    [manager POST:home_allTasks parameters:nil progress:^(NSProgress * _Nonnull uploadProgress) {
+        
+    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        if ([responseObject[@"flag"] integerValue] == 0)
+        {
+            
+        }
+        else
+        {
+            
+        }
+        
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        //        NSLog(@"%@",error.localizedDescription);
+    }];
+}
+
 @end
