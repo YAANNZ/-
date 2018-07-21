01eb2a5fbae84dbb423588d1726ca98578c87a5f
YAANNZ <zhuynchn@163.com>
2018-07-21 23:33:32 +0800

    home

--- PositivePersons/Project/Home/TableViewProtocol/PPSHomeTableViewDelegate.m
+++ PositivePersons/Project/Home/TableViewProtocol/PPSHomeTableViewDelegate.m
@@ -12,17 +12,36 @@
 
 - (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
 {
-    UILabel *headerLabel = [[UILabel alloc] init];
-    NSDictionary *dataDict = self.dataArray[section];
-    headerLabel.textColor = [UIColor blackColor];
-    headerLabel.backgroundColor = [UIColor colorWithRed:237/255.0 green:237/255.0 blue:237/255.0 alpha:1.0];
+    UIView *headerView = [[UIView alloc] init];
+    headerView.backgroundColor = [UIColor colorWithRed:237/255.0 green:237/255.0 blue:237/255.0 alpha:1.0];
     
+    UILabel *headerLabel = [[UILabel alloc] init];
+    headerLabel.textColor = [UIColor blackColor];
+    [headerView addSubview:headerLabel];
+    
+    UIButton *addTaskBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    [addTaskBtn setImage:[UIImage imageNamed:@"add"] forState:UIControlStateNormal];
+    [headerView addSubview:addTaskBtn];
+    
+    // 约束
+    [headerLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(@10);
+        make.top.bottom.right.equalTo(@0);
+    }];
+    [addTaskBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(@5);
+        make.bottom.equalTo(@-5);
+        make.right.equalTo(@-10);
+    }];
+    
+    // 赋值
+    NSDictionary *dataDict = self.dataArray[section];
     if (section == 0 || [[dataDict objectForKey:PPSHomeTasksAryKey] count] != 0)
     {
         headerLabel.text = [self.dataArray[section] objectForKey:PPSHomeTasksTitleKey];
     }
     
-    return headerLabel;
+    return headerView;
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
